9e97ac7435c3c2403f74c091906e8b6bc293ce90
arzamasov <arzamasov@i40pool.ipd.kit.edu>
2020-01-10 18:47:50 +0100

    small shanges in names

--- results_processing.R
+++ results_processing.R
@@ -14,10 +14,10 @@ results = unwrap(reduceResultsDataTable(fun = reduce))
 pars = unwrap(getJobPars())
 results = ijoin(pars, results)
 
-results[max.be == 1.2, battery := "battery1"]
-results[max.be == 2.4, battery := "battery2"]
-results[max.be == 4, battery := "battery3"]
-results[max.be == 6.5, battery := "battery4"]
+results[max.be == 1.2, battery := "AC Battery"]
+results[max.be == 2.4, battery := "Myreserve Pack"]
+results[max.be == 4, battery := "eco 8.0/4"]
+results[max.be == 6.5, battery := "RESU 6.5"]
 
 save(results, file = paste0(getwd(), "/results.RData"))
 
@@ -61,6 +61,18 @@ for(i in 1: nrow(pivot)){
 #' which will identyfy the BBLH used. The result is in "algo.name" field
 
 results$algo.name <- paste0(results$algo.name, ".", results$mode)
+results[algo.name == "alg.be.first", algo.name := "BE1"]
+results[algo.name == "alg.be.second", algo.name := "BE2"]
+results[algo.name == "alg.nill.na", algo.name := "NILL"]
+results[algo.name == "alg.stepping.lc", algo.name := "LC"]
+results[algo.name == "alg.stepping.ls1", algo.name := "LS1"]
+results[algo.name == "alg.stepping.ls2", algo.name := "LS2"]
+results[algo.name == "alg.stepping.rc", algo.name := "RC"]
+
+#' save and load the result
+
+save(results, file = paste0(getwd(), "/results_ranked.RData"))
+load(paste0(getwd(), "/results_ranked.RData"))
 
 #' average ranks of the BBLHs
 
@@ -87,13 +99,40 @@ write.csv(res, paste0(getwd(), "/results/res_lp.csv"))
 d <- aggregate(results$algo.rank, list(results$algo.name, results$meas.id), mean)
 names(d) <- c("algo.name", "measure", "value")
 res <- cast(d, algo.name~measure, mean)
-res <- t(apply(res, 2, rank))
-write.csv(res, paste0(getwd(), "/results/res_meas.csv"))
+res <- as.data.frame(t(apply(res, 2, rank)))
+res$measure <- rownames(res)
+res <- data.table(res)
+
+res$diff <- ifelse(grepl("diff", res$measure), "y", "n")
+res$measure <- gsub(".diff", "", res$measure)
+res$measure <- gsub(".orig", "", res$measure)
+res[measure == "ce", measure := "H"]
+res[measure == "cs", measure := "CS"]
+res[measure == "dc.reg", measure := "R^2_2"]
+res[measure == "dc.lpo", measure := "R^2_p"]
+res[measure == "er.nozeros", measure := "ER_{nz}"]
+res[measure == "er.zeros", measure := "Cs"]
+res[measure == "fm.ed", measure := "FM_{ed}"]
+res[measure == "fm.fm", measure := "FM"]
+res[measure == "fm.rfm", measure := "FM_r"]
+res[measure == "kd", measure := "K"]
+res[measure == "kl", measure := "KL"]
+res[measure == "lv", measure := "LV"]
+res[measure == "mi.bin", measure := "MI_b"]
+res[measure == "mi.iid", measure := "MI_i"]
+res[measure == "mi.ms", measure := "MI_s"]
+res[measure == "mi.mns", measure := "MI_v"]
+res[measure == "rc.v", measure := "RU_v"]
+res[measure == "rc.w", measure := "RU_w"]
+res[measure == "tvd", measure := "TVD"]
+res$measure <- paste0("$\\mathop{", res$measure, "}$")
+
+write.csv(res, paste0(getwd(), "/results/res_meas.csv"), row.names = FALSE)
 
 #' some additional plots
 
-d <- results[algo.name == "alg.be.first",]
+d <- results[algo.name == "BE1",]
 barplot(table(d$algo.rank)/sum(table(d$algo.rank)))
 
-d <- results[algo.name == "alg.nill.na",]
+d <- results[algo.name == "NILL",]
 barplot(table(d$algo.rank)/sum(table(d$algo.rank)))
